7dae46f0d6493b2a3bba5f895eb658e5a7f9743b
Amruta <pamruta@live.com>
2017-07-20 22:31:01 +0530

    Adding voice interface to Raspberry Pi

---
+++ Talking-Pi.py
@@ -0,0 +1,52 @@
+
+# function to get current local time
+def get_time():
+	# importing python libraries to capture date and time
+	from time import asctime, localtime, time
+
+	# get current local time
+	current_time = asctime(localtime(time()))
+	[day, month, date, time_stamp, year] = current_time.split(" ")
+	[hr, mins, secs] = time_stamp.split(":")
+
+	utterance = "Current Time is " + hr + " " + mins
+	return utterance
+
+# calling text to speech api
+def speak_polly(text_utterance):
+	# using aws polly for speech synthesis
+	call_polly = "aws polly synthesize-speech --voice-id Joanna --output-format mp3 output.mp3 --text \"" + text_utterance + "\""
+
+	# run the command
+	import os
+	os.system(call_polly)
+
+	# play audio
+	os.system("afplay output.mp3")
+
+# gets the weather data from Open Weather Map
+def get_weather(city):
+	import urllib2
+	import json
+
+	# api-key for open weather map
+	API_KEY = open("weather-api.key").read()
+	web_url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=" + API_KEY
+	weather_data = urllib2.urlopen(web_url).read()
+	weather = json.loads(weather_data)
+
+	utterance = "Outside Temperature is " + str(weather['main']['temp']) + " degree celsius"
+	return utterance
+
+import time
+while(1):
+	# tell the current time
+	tell_time = get_time()
+	speak_polly(tell_time)
+
+	# tell the current weather 
+	tell_weather = get_weather("bangalore,india")
+	speak_polly(tell_weather)
+
+	# set timer 
+	time.sleep(30)
