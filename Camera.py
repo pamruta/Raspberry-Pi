ad55fb5e0ea92c678639c6e0893874503dca9ce2
Amruta <amruta@Amrutas-MacBook-Air.local>
2018-01-26 15:58:32 +0530

    added script Camera.py

---
+++ Camera.py
@@ -0,0 +1,12 @@
+
+# python script to test Raspberry Pi camera
+
+import picamera
+from time import sleep
+
+camera = picamera.PiCamera()
+camera.vflip=True
+
+camera.start_preview(fullscreen=True)
+sleep(60)
+camera.stop_preview()
